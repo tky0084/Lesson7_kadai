991fb6930db254acbc97890f156f96d626b78936
tky0084 <tky0084@example.com>
2022-08-09 10:16:44 +0000

    課題提出_n回目

--- resources/views/task/completed_list.blade.php
+++ resources/views/task/completed_list.blade.php
@@ -25,7 +25,7 @@
         {{-- {{ $list->links() }} --}}
         現在 {{ $list->currentPage() }} ページ目<br>
         @if ($list->onFirstPage() === false)
-            <a href="/task/list">最初のページ</a>
+            <a href="/completed_tasks/list">最初のページ</a>
         @else
             最初のページ
         @endif
